9792265d6420528f476b6e7ac7c1df1583fa8e8c
Cauan Nascimento <70200313+SLEEPiNHOO@users.noreply.github.com>
2021-11-19 19:44:35 -0300

    Update no ex001.py
    O código foi formatado.

--- Exercicios/ex011.py
+++ Exercicios/ex011.py
@@ -1,11 +1,16 @@
-nome =  str(input("Digite o seu nome: ")).strip() # Use o .strip() para eliminar os espacos antes e depois de uma cadeia de caracteres, isso eh util para nomes de pessoas.
+# Use o .strip() para eliminar os espacos antes e depois de uma cadeia de caracteres, isso eh util para nomes de pessoas.
+nome = str(input("Digite o seu nome: ")).strip()
 
 print("Um momento, estamos analisando seu nome...")
 print(f"Seu nome no formato original e: {nome}")
-print(f"Em mauisculas fica assim: {nome.upper()}") #variavel.upper() deixa todos os caracteres da cadeia em maiusculo
-print(f"Em minusculas, fica assim: {nome.lower()}") #variavel.lower() deixa todas as cadeias de caracteres em minusculo
-print("Seu nome tem {} letras.".format(len(nome) - nome.count(" "))) # len(variavel) eh usado para contar quantos caracteres tem em uma cadeia (contando os espacos)
+# variavel.upper() deixa todos os caracteres da cadeia em maiusculo
+print(f"Em mauisculas fica assim: {nome.upper()}")
+# variavel.lower() deixa todas as cadeias de caracteres em minusculo
+print(f"Em minusculas, fica assim: {nome.lower()}")
+# len(variavel) eh usado para contar quantos caracteres tem em uma cadeia (contando os espacos)
+print("Seu nome tem {} letras.".format(len(nome) - nome.count(" ")))
 # para tirar os espacos, basta subtrair len(variavel) - nome.count(" ") >>> ele vai contar quantos desse caractere colocado entre parenteses tem na cadeia (espaco conta), e vai subtrair no resultado.
-print("Seu primeiro nome tem {}".format(nome.find(" "))) #esse nome.find(" "), ele vai achar o primeiro espaco e vai cortar ele e todos os caracteres em diante, util para achar o primeiro nome de uma pessoa.
+# esse nome.find(" "), ele vai achar o primeiro espaco e vai cortar ele e todos os caracteres em diante, util para achar o primeiro nome de uma pessoa.
+print("Seu primeiro nome tem {}".format(nome.find(" ")))
 split = nome.split()
 print(f"E antes que eu me esqueca, seu primeiro nome eh {split[0]}")
