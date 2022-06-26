f1fe52e5048182668059522e4c2d2a1c75c43d15
Cauan Nascimento <70200313+sleepinhoo@users.noreply.github.com>
2022-06-26 00:57:37 -0300

    Update ex007.py
    O exercicio 007 foi colorificado

--- Exercicios/colorificados/ex007.py
+++ Exercicios/colorificados/ex007.py
@@ -1,8 +1,8 @@
 print("Aviso: use o ponto final em vez de vírgula para representar números decimais.")
 
-nota1 = float(input("Digite nota 1: "))
+nota1 = float(input("\033[0;35mDigite nota 1:/033[m "))
 nota2 = float(input("Digite a nota 2: "))
 
 med = (nota1 + nota2) / 2
 
-print(f"A média do aluno foi {med}")
+print(f"A média do aluno foi \033[4;34m{med}\033[m")
